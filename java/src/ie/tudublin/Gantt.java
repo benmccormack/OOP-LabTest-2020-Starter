545d44b4186d4af6ade53f1654c9b1368aa3710b
benmccormack <c19459424@mytudublin.ie>
2021-04-18 19:47:23 +0100

    loadTasks Function and Array List done

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,18 +1,31 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gantt extends PApplet
 {	
+	//An arrayList to hold instances of the task class
+	ArrayList<Task> tasks = new ArrayList<Task>();
 	
 	public void settings()
 	{
 		size(800, 600);
 	}
 
+	//load tasks method which populates the ArrayList from the .csv file
 	public void loadTasks()
 	{
-		
+		Table table = loadTable("tasks.csv", "header");
+
+		for(TableRow row:table.rows())
+		{
+			Task t = new Task(row);
+			tasks.add(t);
+		}
 	}
 
 	public void printTasks()
